71f62bd4bf82563a0007368e2a729d877aaba101
Filip Sochal <fsochal@gmail.com>
2017-03-24 19:13:18 +0100

    Created controller for main Window.

---
+++ client-desktop/src/main/java/pl/put/poznan/whereismymoney/controllers/MainWindowController.java
@@ -0,0 +1,24 @@
+package pl.put.poznan.whereismymoney.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainWindowController implements Controller {
+    @FXML
+    private BorderPane mainWindow;
+
+    @FXML
+    private void performClose() {
+        getPrimaryStage().close();
+    }
+
+    @FXML
+    private void performMinimize() {
+        getPrimaryStage().setIconified(true);
+    }
+
+    private Stage getPrimaryStage() {
+        return (Stage) mainWindow.getScene().getWindow();
+    }
+}
