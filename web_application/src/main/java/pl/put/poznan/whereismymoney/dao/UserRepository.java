685f16af9b27fc6d41d107b153236b81ea80c187
Filip Sochal <fsochal@gmail.com>
2017-04-19 18:11:05 +0200

    Some minor bugfix on server (there's no possibility two users with same login/email exist).

--- web_application/src/main/java/pl/put/poznan/whereismymoney/dao/UserRepository.java
+++ web_application/src/main/java/pl/put/poznan/whereismymoney/dao/UserRepository.java
@@ -6,6 +6,6 @@ import pl.put.poznan.whereismymoney.model.User;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByUsername(String username);
-    List<User> findByEmail(String email);
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
