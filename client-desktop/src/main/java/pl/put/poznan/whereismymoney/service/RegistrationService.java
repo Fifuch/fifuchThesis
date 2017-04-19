0bcf1fd519d2474d3e3a90c82c3cc1337fe607e8
Filip Sochal <fsochal@gmail.com>
2017-04-19 18:05:37 +0200

    Started replacing dao with server communicator and made registration great again.

--- client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/RegistrationService.java
+++ client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/RegistrationService.java
@@ -1,33 +1,53 @@
 package pl.put.poznan.whereismymoney.service;
 
-import pl.put.poznan.whereismymoney.dao.UserDao;
-import pl.put.poznan.whereismymoney.model.User;
-import pl.put.poznan.whereismymoney.security.SessionManager;
+import pl.put.poznan.whereismymoney.http.ServerCommunicator;
+import pl.put.poznan.whereismymoney.http.util.ResponseCodes;
+import pl.put.poznan.whereismymoney.injector.annotation.Registration;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationService {
-    private SessionManager sessionManager;
-    private UserDao userDao;
+    private MessageDigest sha256;
+    private ServerCommunicator serverCommunicator;
+    private final String registrationAddress;
 
     @Inject
-    public RegistrationService(SessionManager sessionManager, UserDao userDao) {
-        this.sessionManager = sessionManager;
-        this.userDao = userDao;
+    public RegistrationService(MessageDigest sha256, ServerCommunicator serverCommunicator,
+                               @Registration String registrationAddress) {
+        this.sha256 = sha256;
+        this.serverCommunicator = serverCommunicator;
+        this.registrationAddress = registrationAddress;
     }
 
-    public boolean performRegistration(String username, String email, String password, String retypedPassword) {
-        boolean operationSuccessful = true;
-        if (userDao.getByUsername(username) != null) {
-            operationSuccessful = false;
+    public String performRegistration(String username, String email, String password, String retypedPassword) {
+        String response = ResponseCodes.NONIDENTICAL_PASSWORDS.toString();
+        if (!isAnyFieldEmpty(username, email, password) && password.equals(retypedPassword)) {
+            try {
+                Map<String, String> parameters = prepareRegistrationParameters(username, email, password);
+                response = serverCommunicator.sendMessageAndWaitForResponse(registrationAddress, parameters);
+            } catch (IOException e) {
+                response = ResponseCodes.COMMUNICATION_ERROR.toString();
+            }
         }
-        if (userDao.getByEmail(email) != null) {
-            operationSuccessful = false;
-        }
-        if (password.equals(retypedPassword) && operationSuccessful) {
-            User user = new User(username, email, sessionManager.digestPassword(password));
-            userDao.saveOrUpdate(user);
-        }
-        return operationSuccessful;
+        return response;
+    }
+
+    private boolean isAnyFieldEmpty(String username, String email, String password) {
+        return username == null || email == null || password == null;
+    }
+
+    private Map<String, String> prepareRegistrationParameters(String username, String email,
+                                                              String password) throws UnsupportedEncodingException {
+        String passwordDigest = new String(sha256.digest(password.getBytes("UTF-8")), "UTF-8");
+        Map<String, String> parameters = new HashMap<>(3);
+        parameters.put("username", username);
+        parameters.put("email", email);
+        parameters.put("password", passwordDigest);
+        return parameters;
     }
 }
