1719f3a8995f3f8a2428ccd719ca8020a2fc8ba0
Filip Sochal <fsochal@gmail.com>
2017-04-20 17:29:29 +0200

    Reworked session manager (including some renaming; that's why UserSettingsService is here), updated registration process, added working logon service.

--- client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/LogonService.java
+++ client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/LogonService.java
@@ -1,34 +1,94 @@
 package pl.put.poznan.whereismymoney.service;
 
+import com.google.gson.Gson;
+import pl.put.poznan.whereismymoney.http.ServerCommunicator;
+import pl.put.poznan.whereismymoney.injector.annotation.Logon;
 import pl.put.poznan.whereismymoney.model.User;
 import pl.put.poznan.whereismymoney.security.SessionManager;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SignInService {
+public class LogonService {
     private SessionManager sessionManager;
+    private Gson gson;
+    private ServerCommunicator serverCommunicator;
+    private final String logonAddress;
 
     @Inject
-    public SignInService(SessionManager sessionManager) {
+    public LogonService(SessionManager sessionManager, Gson gson, ServerCommunicator serverCommunicator,
+                        @Logon String logonAddress) {
         this.sessionManager = sessionManager;
+        this.gson = gson;
+        this.serverCommunicator = serverCommunicator;
+        this.logonAddress = logonAddress;
     }
 
-    public boolean performSignIn(String username, String password) {
-        byte[] passwordDigest = sessionManager.digestPassword(password);
+    public boolean performLogon(String username, String password) {
+        boolean logonSuccessful = false;
+        if (!isAnyFieldEmpty(username, password)) {
+            byte[] salt = getSalt(username);
+            byte[] passwordDigest = sessionManager.generatePasswordDigest(password, salt);
+            logonSuccessful = validateCredentials(username, passwordDigest);
+        }
+        return logonSuccessful;
+    }
+
+    private boolean isAnyFieldEmpty(String username, String password) {
+        return username.equals("") || password.equals("");
+    }
+
+    private byte[] getSalt(String username) {
+        String saltText = getSaltText(username);
+        byte[] salt = new byte[0];
+        if (!saltText.equals("")) {
+            salt = gson.fromJson(saltText, byte[].class);
+        }
+        return salt;
+    }
+
+    private String getSaltText(String username) {
+        Map<String, String> parameters = new HashMap<>(1);
+        parameters.put("username", username);
+        String saltText;
+        try {
+            saltText = serverCommunicator.sendMessageAndWaitForResponse(logonAddress + "/salt", parameters);
+        } catch (IOException e) {
+            saltText = "[]";
+        }
+        return saltText;
+    }
+
+    private boolean validateCredentials(String username, byte[] passwordDigest) {
+        boolean operationResult = false;
+        if (passwordDigest.length > 0) {
+            generateSessionKey(username, passwordDigest);
+            operationResult = validateOnServer();
+        }
+        return operationResult;
+    }
+
+    private void generateSessionKey(String username, byte[] passwordDigest) {
         User user = new User();
         user.setUsername(username);
         user.setPassword(passwordDigest);
         sessionManager.setUserData(user);
-        long id = validate(sessionManager.getSessionKey(), username);
-        if (id >= 0) {
-            user.setId(id);
-            return true;
-        } else {
-            return false;
-        }
+        sessionManager.generateSessionKey();
     }
 
-    private long validate(byte[] sessionKey, String username) {
-        return 0;
+    private boolean validateOnServer() {
+        Map<String, String> parameters = new HashMap<>(2);
+        parameters.put("username", sessionManager.getUserData().getUsername());
+        String sessionKey = gson.toJson(sessionManager.getSessionKey());
+        parameters.put("sessionKey", sessionKey);
+        String validationResult;
+        try {
+            validationResult = serverCommunicator.sendMessageAndWaitForResponse(logonAddress + "/verify", parameters);
+        } catch (IOException e) {
+            validationResult = "false";
+        }
+        return gson.fromJson(validationResult, boolean.class);
     }
 }
--- client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/RegistrationService.java
+++ client-desktop/src/main/java/pl/put/poznan/whereismymoney/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package pl.put.poznan.whereismymoney.service;
 
+import com.google.gson.Gson;
 import pl.put.poznan.whereismymoney.http.ServerCommunicator;
 import pl.put.poznan.whereismymoney.http.util.ResponseCodes;
 import pl.put.poznan.whereismymoney.injector.annotation.Registration;
@@ -8,27 +9,33 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegistrationService {
-    private MessageDigest sha256;
+    private MessageDigest messageDigest;
     private ServerCommunicator serverCommunicator;
     private final String registrationAddress;
+    private final SecureRandom secureRandom;
+    private Gson gson;
 
     @Inject
-    public RegistrationService(MessageDigest sha256, ServerCommunicator serverCommunicator,
-                               @Registration String registrationAddress) {
-        this.sha256 = sha256;
+    public RegistrationService(MessageDigest messageDigest, ServerCommunicator serverCommunicator,
+                               @Registration String registrationAddress, SecureRandom secureRandom, Gson gson) {
+        this.messageDigest = messageDigest;
         this.serverCommunicator = serverCommunicator;
         this.registrationAddress = registrationAddress;
+        this.secureRandom = secureRandom;
+        this.gson = gson;
     }
 
     public String performRegistration(String username, String email, String password, String retypedPassword) {
         String response = ResponseCodes.NONIDENTICAL_PASSWORDS.toString();
         if (!isAnyFieldEmpty(username, email, password) && password.equals(retypedPassword)) {
             try {
-                Map<String, String> parameters = prepareRegistrationParameters(username, email, password);
+                byte[] salt = generateSalt();
+                Map<String, String> parameters = prepareRegistrationParameters(username, email, password, salt);
                 response = serverCommunicator.sendMessageAndWaitForResponse(registrationAddress, parameters);
             } catch (IOException e) {
                 response = ResponseCodes.COMMUNICATION_ERROR.toString();
@@ -37,17 +44,30 @@ public class RegistrationService {
         return response;
     }
 
+    private byte[] generateSalt() {
+        byte[] result = new byte[32];
+        secureRandom.nextBytes(result);
+        return result;
+    }
+
     private boolean isAnyFieldEmpty(String username, String email, String password) {
         return username == null || email == null || password == null;
     }
 
-    private Map<String, String> prepareRegistrationParameters(String username, String email,
-                                                              String password) throws UnsupportedEncodingException {
-        String passwordDigest = new String(sha256.digest(password.getBytes("UTF-8")), "UTF-8");
+    private Map<String, String> prepareRegistrationParameters(String username, String email, String password,
+                                                              byte[] salt) throws UnsupportedEncodingException {
+        byte[] passwordDigest = generateDigest(password, salt);
         Map<String, String> parameters = new HashMap<>(3);
         parameters.put("username", username);
         parameters.put("email", email);
-        parameters.put("password", passwordDigest);
+        parameters.put("password", gson.toJson(passwordDigest));
+        parameters.put("salt", gson.toJson(salt));
         return parameters;
+    }
+
+    private byte[] generateDigest(String password, byte[] salt) throws UnsupportedEncodingException {
+        messageDigest.update(password.getBytes("UTF-8"));
+        messageDigest.update(salt);
+        return messageDigest.digest();
     }
 }
