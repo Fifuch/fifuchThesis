4f532f27fc747d24c399178e52795b9f36a1a9f2
Kheldarek <psendrowski@gmail.com>
2017-04-27 05:46:43 +0200

    + added androidTransaction methods to overcome java.util.Date restrictions

--- web_application/src/main/java/pl/put/poznan/whereismymoney/service/secured/TransactionService.java
+++ web_application/src/main/java/pl/put/poznan/whereismymoney/service/secured/TransactionService.java
@@ -1,6 +1,7 @@
 package pl.put.poznan.whereismymoney.service.secured;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,14 @@ import pl.put.poznan.whereismymoney.dao.CategoryRepository;
 import pl.put.poznan.whereismymoney.dao.TransactionRepository;
 import pl.put.poznan.whereismymoney.model.Budget;
 import pl.put.poznan.whereismymoney.model.Transaction;
+import pl.put.poznan.whereismymoney.model.TransactionAndroid;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import java.text.DateFormat;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -53,6 +59,33 @@ public class TransactionService {
         return Encryption.encryptParameter(response,aesKey,ivParameter);
     }
 
+    @PostMapping("/getByBudgetAndroid")
+    public String getTransactionsByBudgetAndroid(String sessionKey, String username, String budgetId, String cipherKey, String iv) {
+        SecretKey aesKey = Encryption.decryptAesKey(cipherKey, rsaKeyManager.getPrivateKey());
+        IvParameterSpec ivParameter = Encryption.decryptIv(iv, rsaKeyManager.getPrivateKey());
+        long decryptedBudgetId = Long.parseLong(Encryption.decryptStringParameter(budgetId,aesKey,ivParameter));
+
+        Budget budget = budgetRepository.findOne(decryptedBudgetId);
+        String response = "[]";
+        if (budget.getOwner().getUsername().equals(Encryption.decryptStringParameter(username,aesKey,ivParameter))) {
+            List<Transaction> transactions = transactionRepository.findByRelatedBudgetId(decryptedBudgetId);
+            List<TransactionAndroid> transactionAndroidList = new ArrayList<>();
+            for(Transaction transaction: transactions){
+                TransactionAndroid transactionAndroid =new TransactionAndroid();
+
+                transactionAndroid.setTransactionCategory(transaction.getTransactionCategory());
+                transactionAndroid.setRelatedBudget(transaction.getRelatedBudget());
+                transactionAndroid.setAmount(transaction.getAmount());
+                transactionAndroid.setId(transaction.getId());
+                transactionAndroid.setTransactionType(transaction.getTransactionType());
+                transactionAndroid.setTransactionDate(Date.from(transaction.getTransactionDate().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+                transactionAndroidList.add(transactionAndroid);
+            }
+            response = gson.toJson(transactionAndroidList);
+        }
+        return Encryption.encryptParameter(response,aesKey,ivParameter);
+    }
+
     @PostMapping("/getByCategory")
     public String getTransactionsByCategory(String sessionKey, String username, String categoryId, String cipherKey, String iv) {
         SecretKey aesKey = Encryption.decryptAesKey(cipherKey, rsaKeyManager.getPrivateKey());
@@ -67,6 +100,28 @@ public class TransactionService {
             response = gson.toJson(transactions);
         }
         return Encryption.encryptParameter(response,aesKey,ivParameter);
+    }
+
+    @PostMapping("/addAndroid")
+    public String addTransactionAndroid(String sessionKey, String username, String transaction, String cipherKey, String iv) {
+        SecretKey aesKey = Encryption.decryptAesKey(cipherKey, rsaKeyManager.getPrivateKey());
+        IvParameterSpec ivParameter = Encryption.decryptIv(iv, rsaKeyManager.getPrivateKey());
+        transaction = Encryption.decryptStringParameter(transaction,aesKey,ivParameter);
+        Gson tmp  = new GsonBuilder().setDateFormat(DateFormat.FULL,DateFormat.FULL).create();
+
+        TransactionAndroid newTransactionAndroid = tmp.fromJson(transaction, TransactionAndroid.class);
+        Transaction newTransaction =new Transaction();
+
+        newTransaction.setAmount(newTransactionAndroid.getAmount());
+        newTransaction.setRelatedBudget(newTransactionAndroid.getRelatedBudget());
+        newTransaction.setTransactionCategory(newTransactionAndroid.getTransactionCategory());
+        newTransaction.setTransactionDate(newTransactionAndroid.getTransactionDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        newTransaction.setTransactionType(newTransactionAndroid.getTransactionType());
+
+        if (newTransaction.getRelatedBudget().getOwner().getUsername().equals(Encryption.decryptStringParameter(username,aesKey,ivParameter))) {
+            return Encryption.encryptParameter(String.valueOf(transactionRepository.save(newTransaction) != null),aesKey,ivParameter);
+        }
+        return Encryption.encryptParameter(String.valueOf(false),aesKey,ivParameter);
     }
 
     @PostMapping("/add")
